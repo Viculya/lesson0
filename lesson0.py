b1cefe6f773550f53c85bcb7b414a061e537b9fa
Viculya <Viculya30@yandex.ru>
2024-07-13 22:57:29 +0300

    Add files via upload

--- lesson0.py
+++ lesson0.py
@@ -2,8 +2,8 @@ print("Hello world")
 # "1st program"
 print((9*5)**0.5)
 #"2nd program"
-print((9.99>9.98) or (1000!=1000.1)(9.99>9.98) and (1000!=1000.1))
+print(9.99>9.98) and (1000!=1000.1)
 #"3rd program"
-print(23+67)
+print(((1234%1000)//10)+((5678%1000)//10))
 #"4th program"
-print(13==13 and 42==42)
+print((13.42*100)//100)
